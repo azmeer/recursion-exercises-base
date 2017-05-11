6b2f5323b08fc5724fafa2777d4b7be271d1cb55
azmeer <azmeer.salleh@gmail.com>
2017-05-10 18:22:32 -0700

    Adding jsoninator.stringify()

--- jsoninator.js
+++ jsoninator.js
@@ -4,11 +4,37 @@ const _ = require('underscore'); // the real one! :)
 // const stringify = JSON.stringify;
 // But you don't. So you're going to write it from scratch...
 
-
-
 const stringify = function(obj) {
-  // your code goes here
+  /*
+    We test the object, and based on its type do the following:
+       a string - return the string enclosed in quotes
+       a boolean - return a string of 'true' or 'false'
+       a number - return a string of the number
+       null - return a string of 'null'
+       an object - recursively call stringify on keys and values of the object
+       an array - recursively call stringify on the elements of the array
+  */
+  if (typeof(obj) === 'string') return '\"'+obj+'\"';
+  else if (typeof(obj) === 'number') return obj.toString();
+  else if (typeof(obj) === 'boolean') return obj ? 'true' : 'false';
+  else if (obj === null) return 'null';
+  else if (_.isArray(obj)) return stringifyArray(obj);
+  else if (_.isObject(obj)) return stringifyObject(obj);
+  else {
+    // error
+    return '';
+  }
 };
+
+const stringifyArray = function(obj) {
+  return '[' +  _.map(obj, (element, index) => stringify(element)).join(',') + ']';
+}
+
+const stringifyObject = function(obj) {
+  return '{' +
+    _.map(_.keys(obj), key => stringify(key) + ':' + stringify(obj[key])).join(',')
+    + '}';
+}
 
 module.exports = {
   stringify: stringify
