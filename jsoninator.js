953fd9ba5ef2dc56537c17abade8c73e24fd12a4
azmeer <azmeer.salleh@gmail.com>
2017-05-10 18:24:06 -0700

    Cleaned up jsoninator

--- jsoninator.js
+++ jsoninator.js
@@ -7,23 +7,19 @@ const _ = require('underscore'); // the real one! :)
 const stringify = function(obj) {
   /*
     We test the object, and based on its type do the following:
-       a string - return the string enclosed in quotes
-       a boolean - return a string of 'true' or 'false'
-       a number - return a string of the number
-       null - return a string of 'null'
-       an object - recursively call stringify on keys and values of the object
-       an array - recursively call stringify on the elements of the array
+    a string - return the string enclosed in quotes
+    a boolean - return a string of 'true' or 'false'
+    a number - return a string of the number
+    null - return a string of 'null'
+    an object - recursively call stringify on keys and values of the object
+    an array - recursively call stringify on the elements of the array
   */
   if (typeof(obj) === 'string') return '\"'+obj+'\"';
   else if (typeof(obj) === 'number') return obj.toString();
   else if (typeof(obj) === 'boolean') return obj ? 'true' : 'false';
   else if (obj === null) return 'null';
   else if (_.isArray(obj)) return stringifyArray(obj);
-  else if (_.isObject(obj)) return stringifyObject(obj);
-  else {
-    // error
-    return '';
-  }
+  else return stringifyObject(obj);
 };
 
 const stringifyArray = function(obj) {
